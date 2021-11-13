c96c08d8ca5a10b2ec3aee2abcbca3702509322c
jeodom17 <jeodom17@gmail.com>
2021-11-12 21:21:06 -0500

    define columns in models/Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,42 @@
-// import important parts of sequelize library
+//* import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+
+//* import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+//* Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+//* set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //* define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
@@ -20,4 +47,5 @@ Product.init(
   }
 );
 
+//* --- Export -----
 module.exports = Product;
