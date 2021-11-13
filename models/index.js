da81e17f8e3030f13c11528c568920c78831220c
jeodom17 <jeodom17@gmail.com>
2021-11-12 21:01:13 -0500

    added code to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,30 @@
-// import models
+//* import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//* Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'Cascade',
+});
+//* Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+//* Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
+//* Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+//* Exports
 module.exports = {
   Product,
   Category,
