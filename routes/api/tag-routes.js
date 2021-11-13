5ac0b9dcf3d2a7d4d2f126783717fe3058e8905f
jeodom17 <jeodom17@gmail.com>
2021-11-13 14:04:14 -0500

    route for create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,7 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 //* The `/api/tags` endpoint
   //* find all tags
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
@@ -21,14 +21,33 @@ router.get('/', (req, res) => {
   }
 });
 
+//* find a single tag by its `id`
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [
+        { 
+          model: Product, 
+          through: ProductTag 
+        }
+      ],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//* create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
