d4a0c94b3706c8616648fc6949d18a0d4dfc11e5
jeodom17 <jeodom17@gmail.com>
2021-11-14 18:57:00 -0500

    route fix

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,8 +32,8 @@ router.get('/:id', (req, res) => {
 //* create a new category
 router.post('/', async (req, res) => {
   try {
-    const categories = await Category.create(req.body);
-    res.status(200).json(categories);
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
