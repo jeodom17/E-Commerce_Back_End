85edb77f5e5033e195c0658de1bb2ab1a0f64106
jeodom17 <jeodom17@gmail.com>
2021-11-14 18:50:36 -0500

    fix category put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,15 +29,18 @@ router.get('/:id', (req, res) => {
   .catch((err) => res.status(500).json(err));
 });
 
-router.post('/', (req, res) => {
-  //* create a new category
-  Category.create(req.body)
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
+//* create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categories = await Category.create(req.body);
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//* update a category by its `id` value
 router.put('/:id', (req, res) => {
-  //* update a category by its `id` value
   Category.update(req.body, {
     where: {
       id: req.params.id,
