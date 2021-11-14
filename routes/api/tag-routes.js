ecb25f2e6d2cda3ef0a46e74e09bc13750387c3c
jeodom17 <jeodom17@gmail.com>
2021-11-14 17:00:37 -0500

    error fix

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,7 +41,7 @@ router.get('/:id', async (req, res) => {
 });
 
 //* create a new tag
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const tags = await Tag.create(req.body);
     res.status(200).json(tags);
